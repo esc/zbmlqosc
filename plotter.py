0fcf4ba97a5c2b1dc91506a8681ff3496e66903b
Valentin Haenel <valentin@haenel.co>
2015-05-23 22:51:42 +0200

    update plotter to print data

--- plotter.py
+++ plotter.py
@@ -1,7 +1,6 @@
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 import bcolz
-import numpy as np
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
@@ -9,11 +8,11 @@ ax1 = fig.add_subplot(1, 1, 1)
 
 def animate(i):
     b = bcolz.open('db')
-    yar = [j[0] for j in b[-10:]]
-    xar = np.arange(len(yar))
+    xar = [j[0] for j in b[-10:]]
+    yar = [j[1] for j in b[-10:]]
     ax1.clear()
     ax1.plot(xar, yar)
-    print(yar)
+    print(yar, xar)
 
 ani = animation.FuncAnimation(fig, animate, interval=10)
 plt.show()
